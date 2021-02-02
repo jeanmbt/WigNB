e7cf2006f842d990424746a8ae23ea3c1860674a
Felix Worringen <felixwor@aol.com>
2021-02-02 20:03:29 +0100

    added validations

--- app/models/wig.rb
+++ app/models/wig.rb
@@ -1,4 +1,11 @@
 class Wig < ApplicationRecord
   belongs_to :user
   has_one_attached :photo
+  validates :description, length: { maximum: 1000,
+    too_long: "%{count} characters is the maximum allowed" }
+  validates :color, length: { maximum: 10,
+    too_long: "%{count} characters is the maximum allowed" }
+  validates :name, length: { maximum: 20,
+    too_long: "%{count} characters is the maximum allowed" }
+  validates :color, :name, :description, :material, :style, presence: true
 end
