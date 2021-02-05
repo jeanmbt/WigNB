efabb4077957ec341638abbcb8dc05d1613986d0
Jean Battirola <jeanbattirola@gmail.com>
2021-02-05 15:31:00 +0100

    Trying to add renter id to bookings, in order to reference a user

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,6 +1,7 @@
 class Booking < ApplicationRecord
   belongs_to :user
   belongs_to :wig
+  has_many :renters, class_name: "User", foreign_key: :user_id
   validates :start_date, :end_date, presence: true
   validates :start_date, :end_date, presence: true
 end
