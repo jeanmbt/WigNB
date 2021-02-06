958efe3f3b5adf443a6c5cbf33059a55a15c1fc3
Jean Battirola <jeanbattirola@gmail.com>
2021-02-06 15:37:31 +0100

    fix seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,6 @@
 require "open-uri"
 require 'faker'
-require 'pry-byebug'
+
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
 #
